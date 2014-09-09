9bca1a8eba9e33680c3e53607152e898158a5bc0
akmal <akmal@brighter.com>
2014-09-08 21:13:20 -0700

    initial push

---
+++ js/app.js
@@ -0,0 +1,28 @@
+angular.module("calcApp", []).
+controller("calcController", function($scope){
+	$scope.title = "Waitstaff Calculator";
+
+	$scope.baseMealPrice = 0;
+	$scope.taxRate = 0;
+	$scope.tipPercentage = 0;
+
+	$scope.subtotal= 0.0;
+	$scope.tip = 0.0;
+	$scope.customerTotal = 0.0;
+
+	$scope.tipTotal = 0.0;
+	$scope.mealCount = 0;
+	$scope.averageTip = 0.0;
+
+	$scope.calculate = function(){
+		var taxValue = ($scope.taxRate / 100) * $scope.baseMealPrice;
+		$scope.subtotal = taxValue + $scope.baseMealPrice;
+		$scope.tip = ($scope.tipPercentage / 100) * $scope.subtotal;
+		$scope.customerTotal = $scope.tip + $scope.subtotal;
+
+		$scope.tipTotal = $scope.tipTotal + $scope.tip;
+		$scope.mealCount++;
+		$scope.averageTip = $scope.tipTotal / $scope.mealCount;
+
+	};
+});
